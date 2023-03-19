35ae4506cab4cb2456c9da8c9cd4ffd859edecc7
Andrey <andrey.kononoi@mail.ru>
2023-03-19 19:16:25 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const express = require('express')
+const Sequelize = require('./bd')
+const models = require('./models/models')
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const path = require('path')
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({}))
+app.use('/api', router)
+
+//Обработка ошибок, последний Middleware
+app.use(errorHandler)
+
+async function start() {
+    try {
+        await Sequelize.authenticate()
+        await Sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
